58da17bd4e4441ec0ecd5d24148d7dc145636c07
Rahul P Nath <rahulpnath@gmail.com>
2016-06-28 05:56:54 +1000

    Added chocolatey package nuspec and install scripts

---
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+
+$packageName = 'AsmSpy'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://static.mikehadlow.com/AsmSpy.zip' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' 
+  url           = $url
+  checksum      = '53d3e96213d8f453bc1b5cdbbfb39c78942efef4'
+  checksumType  = 'sha1' 
+}
+
+Install-ChocolateyZipPackage @packageArgs
